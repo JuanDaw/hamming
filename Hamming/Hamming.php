244bdb30bde951c15f0142dcc3d1bbfbd10084b6
Juan Antonio Villoria del Moral <juvi.13.13@gmail.com>
2020-10-13 16:08:12 +0200

    cambios

--- Hamming/Hamming.php
+++ Hamming/Hamming.php
@@ -1,8 +1,14 @@
 <?php
 
-/*
- * Esto es sólo un ESQUELETO para el ejercicio de la "distancia Hamming".
+/**
+ * @author Juan Villoria
+ * @copyright Copyright {c} 2020 Juan Villoria
+ * @license https://www.gnu.org/licenses/gpl-3.0.txt
  */
+
+ /**
+  * Este programa calcula la distancia hamming entre dos cadenas
+  */
 
 function distancia($a, $b)
 {
@@ -10,8 +16,11 @@ function distancia($a, $b)
         return false;
     }
 
-    $sol = 0;
+    if ($a === $b) {
+        return 0;
+    }
 
+    $sol = 0;
     for ($i=0; $i < mb_strlen($a); $i++) { 
         for ($j=0; $j < mb_strlen($b); $j++) { 
             if ($a[$i] = $b[$j]) {
